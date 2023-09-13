02c475caf43583a1041c0058603f4934948e2ead
doubara <ayebadoubara@gmail.com>
2023-09-13 04:07:25 +0100

    fetch top rated movies

--- src/components/FeaturedMovies.jsx
+++ src/components/FeaturedMovies.jsx
@@ -1,14 +1,36 @@
+import { useState, useEffect } from "react";
 
 import MovieCard from "./MovieCard";
 
 const FeaturedMovies = (props) =>{
+    const [movieData, setMovieData] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
 
+    useEffect(()=>{
+
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NWE5YzRkZDA5YmZhYmNjMTAzMTRiOGI1YTU5YTViNCIsInN1YiI6IjY0ZmYxODIwZGI0ZWQ2MTAzMmE2Y2M3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1El547BOzXtxmlszJAjvkLC-81h_bSObXhevpDnjV2A'
+            }
+          };
+          
+          fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                setMovieData(response.results.slice(0, 10))
+                setIsLoading(false);
+            })
+            .catch(err => console.error(err));
+    }, [])
     return (
         <div className='text-black px-16 py-16 border-black'>
             <h2 className='text-4xl pb-4 font-bold'>Feature Movies</h2>
             <div className='grid gap-16 grid-cols-4'>
-            {new Array(10).fill('movie').map((movie, index) =>{
-                return <MovieCard title={index} key={index*Math.random()*210*Math.random()*197} />
+            {!isLoading && movieData.map((movie, index) =>{
+                return <MovieCard movie={movie} title={index} key={index*Math.random()*210*Math.random()*197} />
             })}
             </div>
             
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,68 @@
 import poster from '../assets/poster.svg'
+import heart from '../assets/Heart.svg'
+
 import Rating from './Rating';
+
 import { Link } from 'react-router-dom';
+// Action          28
+// Adventure       12
+// Animation       16
+// Comedy          35
+// Crime           80
+// Documentary     99
+// Drama           18
+// Family          10751
+// Fantasy         14
+// History         36
+// Horror          27
+// Music           10402
+// Mystery         9648
+// Romance         10749
+// Science Fiction 878
+// TV Movie        10770
+// Thriller        53
+// War             10752
+// Western         37
+const GENRE = {
+    28: "Action",
+    12: 'Adventure',
+    16: 'Animation',
+    35: 'Comedy',
+    80: 'Crime',
+    99: 'Docmentary',
+    18: 'Drama',
+    10751: 'Family',
+    14: 'Fantasy',
+    36: 'History',
+    27: 'Horror',
+    10402: 'Music',
+    9648: 'Mystery',
+    10749: 'romance',
+    878: 'Science Fiction',
+    53: 'Thriller',
+    10752: 'War',
+    37: 'Western',
+}
 
-const MovieCard = (props)=>{
+const MovieCard = ({title, movie})=>{
 
+    console.log(movie);
     return (
-        <Link to={`movie/${props.title}`} data-testid='movie-card' className='border hover:scale-105'>
+        <Link to={`movie/${movie.id}`} data-testid='movie-card' className='border relative hover:scale-105'>
                     <div>
-                        <img data-testid='movie-poster' className='w-full' src={poster} alt="" />
+                        <img data-testid='movie-poster' className='w-full' src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
                     </div>
                     <div className='px-4 py-3'>
-                        <p data-testid='movie-release-date' className='text-gray-500'>USA 2005</p>
-                        <h2 data_testid='movie-title' className='text-xl'>Batman Begins</h2>
+                        <p data-testid='movie-release-date' className='text-gray-500'>{movie.release_date.slice(0, 4)}</p>
+                        <h2 data_testid='movie-title' className='text-xl'>{movie.title}</h2>
                         <Rating imdcScore='82.0' tomatoeScore='87%'></Rating>
-                        <p className='text-gray-500'>action adventure</p>
+                        <p className='text-gray-500'>{movie.genre_ids.map(genre_id=>{
+                            return <span className='px-1' key={genre_id}>{GENRE[genre_id]}</span>
+                        })}</p>
                     </div>
-                    
+                    <button className='w-12 h-12 rounded-full absolute -top-2 -right-3'>
+                        <img src={heart} alt="" />
+                    </button>
                 </Link>
     )
 };
