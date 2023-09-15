0be8b2455413369b2f6ed7c7f784759dcce6079c
Kieri Ayibadoubara <ayebadoubara@gmail.com>
2023-09-15 11:02:46 +0100

    Update MovieDetailsCard.jsx spacing inconsistency
    worked on padding issue

--- src/components/MovieDetailsCard.jsx
+++ src/components/MovieDetailsCard.jsx
@@ -4,11 +4,11 @@ const MovieDetailsCard = ({movie}) =>{
         <div className="h-1/2">
             <img className='rounded-lg w-full  object-cover' src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="" />
         </div>
-        <p className="py-6 leading-loose flex gap-2 flex-gap-2 md:flex-col text-xl">
-            <span className='pr-2' data-testid='movie-title'>{movie?.title} </span>
-            <span className='px-2' data-testid='movie-release-date'>{movie?.release_date}</span> 
+        <p className="py-6 leading-loose flex gap-2 flex-gap-2 md:flex-col text-xl items-center">
+            <span data-testid='movie-title'>{movie?.title} </span>
+            <span data-testid='movie-release-date'>{movie?.release_date}</span> 
             <span> PG-13 </span> 
-            <span className='px-2' data-testid='movie-runtime'>{movie.runtime}</span>
+            <span className= data-testid='movie-runtime'>{movie.runtime}</span>
             <span className="text-red-400 border-grey-200 rounded-full p-4">{movie.genres.map(genre=>{
             return <span key={genre*Math.random()*876} className="text-red-400 border border-gray-200 rounded-full inline-blok mr-2 p-2">{genre.name}</span>
         })}</span>  
